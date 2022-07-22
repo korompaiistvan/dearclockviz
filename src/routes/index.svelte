7ba79a8ed53c26a9833265887c083fe311594237
István Korompai <istvan.korompai2@agcocorp.com>
2022-07-22 09:52:08 +0000

    centered a div!!!

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<div class='mockupWrapper'>
+    <svg viewBox='0 0 2970 2100' preserveAspectRatio='xMidYMid meet'>
+        <circle cx={2970/2} cy=1050 r=20 stroke='black'></circle>
+    </svg>
+</div>
+
+<style>
+    .mockupWrapper {
+        --innerPadding: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: calc(100vh - 2 * var(--innerPadding));
+        padding: var(--innerPadding);
+    }
+
+    svg {
+        border: 0.5px solid #a8a8a8;
+        height: 100%;
+    }
+</style>
