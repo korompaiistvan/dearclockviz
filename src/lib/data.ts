3c5eb0f2ba2a31b717732cdc68e14ef8f4d7d7ff
István Korompai <istvan.korompai2@agcocorp.com>
2022-07-26 15:12:11 +0000

    updated Tamas schedule

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -2,17 +2,28 @@ import type { rawTimeData } from './types';
 
 export const data: rawTimeData = {
 	Tamas: [
-		{ start: '00:00', end: '06:30', type: 'sleep' },
-		{ start: '06:30', end: '08:00', type: 'unpaidWork' },
+		{ start: '00:00', end: '02:30', type: 'sleep' },
+		{ start: '02:30', end: '03:15', type: 'unpaidWork' },
+		{ start: '03:15', end: '06:00', type: 'sleep' },
+		{ start: '06:00', end: '06:30', type: 'unpaidWork' },
+		{ start: '06:30', end: '07:30', type: 'fun' },
+		{ start: '07:30', end: '08:00', type: 'unpaidWork' },
 		{ start: '08:00', end: '08:30', type: 'commute' },
-		{ start: '08:30', end: '12:00', type: 'paidWork' },
-		{ start: '12:00', end: '12:45', type: 'fun' }, // ebéd
-		{ start: '12:45', end: '16:15', type: 'paidWork' },
-		{ start: '16:15', end: '17:30', type: 'unpaidWork' },
-		{ start: '17:30', end: '18:00', type: 'fun' },
+		{ start: '08:30', end: '09:00', type: 'unpaidWork' },
+		{ start: '09:00', end: '11:00', type: 'paidWork' },
+		{ start: '11:00', end: '11:30', type: 'fun' },
+		{ start: '11:30', end: '12:00', type: 'paidWork' },
+		{ start: '12:00', end: '13:00', type: 'fun' },
+		{ start: '13:00', end: '15:00', type: 'paidWork' },
+		{ start: '15:00', end: '15:30', type: 'fun' },
+		{ start: '15:30', end: '16:00', type: 'commute' },
+		{ start: '16:00', end: '17:00', type: 'fun' },
+		{ start: '17:00', end: '18:00', type: 'unpaidWork' },
 		{ start: '18:00', end: '19:00', type: 'paidWork' },
-		{ start: '19:00', end: '21:30', type: 'fun' },
-		{ start: '21:30', end: '24:00', type: 'sleep' }
+		{ start: '19:00', end: '19:30', type: 'fun' },
+		{ start: '19:30', end: '21:00', type: 'unpaidWork' },
+		{ start: '21:00', end: '22:00', type: 'fun' },
+		{ start: '22:00', end: '24:00', type: 'sleep' }
 	],
 	Julcsi: [
 		{ start: '00:00', end: '00:30', type: 'fun' },
--- src/lib/styleMap.ts
+++ src/lib/styleMap.ts
@@ -3,8 +3,8 @@ const radiusFactor = 1;
 
 export const styleMap: StyleMap = {
 	sleep: { r: 8.5 * radiusFactor, fill: '#4682B4' }, //confirmed
-	paidWork: { r: 27.5 * radiusFactor, fill: '#bc7361' }, //confirmed
+	paidWork: { r: 27.5 * radiusFactor, fill: '#c0c0c0' }, //confirmed
 	fun: { r: 26.25 * radiusFactor, fill: '#ffd55f' }, //confirmed
-	unpaidWork: { r: 27.5 * radiusFactor, fill: '#C0C0C0' }, //confirmed
+	unpaidWork: { r: 27.5 * radiusFactor, fill: '#bc7361' }, //confirmed
 	commute: { r: 13 * radiusFactor, fill: '#C0C0C0' }
 };
