f19c271123b3935174dab3af1cb6ff8dc81f5465
István Korompai <istvan.korompai2@agcocorp.com>
2022-07-31 09:56:23 +0000

    reworked control panel slightly

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -3,26 +3,22 @@
     import ControlPanelRow from './ControlPanelRow.svelte';
 </script>
 <div class='controlWrapper'>
-    <ControlPanelRow>
-        <label for='bandSpacing'>Band spacing</label>
+    <ControlPanelRow labelFor='bandSpacing' labelString='Band spacing' value='{($bandSpacing/10).toFixed(2)}mm'>
         <input id='bandSpacing' type='range' min=0 max=300 bind:value={$bandSpacing}>
     </ControlPanelRow>
-    <ControlPanelRow>
-        <label for='timeGranularity'>Minutes per dot</label>
+    <ControlPanelRow labelFor='dialPaddingFactor' labelString='Dial padding' value='{$dialPaddingFactor.toFixed(2)}x'>
+        <input id='dialPaddingFactor' type='range' min=0.0 max=3.0  step=0.05 bind:value={$dialPaddingFactor}>
+    </ControlPanelRow>
+    <ControlPanelRow labelFor='bandGroupSpacingFactor' labelString='Space between two circles' value='{$bandGroupSpacingFactor.toFixed(2)}x'>
+        <input id='bandGroupSpacingFactor' type='range' min=0.0 max=3.0 step=0.05 bind:value={$bandGroupSpacingFactor}>
+    </ControlPanelRow>
+    <ControlPanelRow labelFor='timeGranularity' labelString='Minutes per dot'>
         <select id='timeGranularity' bind:value={$timeGranularity}>
             <option value={5}>5</option>
             <option value={10}>10</option>
             <option value={15}>15</option>
             <option value={20}>20</option>
         </select>
-    </ControlPanelRow>
-    <ControlPanelRow>
-        <label for='dialPaddingFactor'>Padding between dial and bands</label>
-        <input id='dialPaddingFactor' type='range' min=0.0 max=3.0  step=0.05 bind:value={$dialPaddingFactor}>
-    </ControlPanelRow>
-    <ControlPanelRow>
-        <label for='bandGroupSpacingFactor'>Space between two circles</label>
-        <input id='bandGroupSpacingFactor' type='range' min=0.0 max=3.0 step=0.05 bind:value={$bandGroupSpacingFactor}>
     </ControlPanelRow>
 </div>
 <style>
@@ -31,5 +27,6 @@
         flex-direction: column;
         justify-content: start;
         margin-top: 8rem;
+        width: 20%;
     }
 </style>
--- src/lib/components/ControlPanelRow.svelte
+++ src/lib/components/ControlPanelRow.svelte
@@ -1,12 +1,35 @@
+<script lang="ts">
+    export let labelFor: string;
+    export let labelString: string;
+    export let value: string | undefined = undefined;
+
+</script>
 <div class='cpRow'>
-    <slot></slot>
+    <div class='header'>
+    <label for={labelFor}>{labelString}</label>
+    {#if value}
+        <p>{value}</p>
+    {:else}
+        <slot />
+    {/if}
+
+    </div>
+    {#if value}
+        <slot></slot>
+    {/if}
 </div>
 <style>
-    .cpRow {
+    .header {
         display: flex;
         flex-direction: row;
         justify-content: space-between;
-        margin: 0.6rem;
+        margin: 0.2rem;
+    }
+    .cpRow {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        margin: 0.8rem;
         font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     }
 </style>
